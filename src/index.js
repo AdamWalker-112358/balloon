b8e40d98752130ed2c8ae9d662aefbef3d0c0ef8
AdamWalker-Tonkean <adam.walker@tonkean.com>
2022-09-02 14:00:04 +0100

    Blowing up the balloon

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const balloon = document.querySelector('p');
+
+
+function inflate(balloon) {
+    let fontSize = window.getComputedStyle(balloon).fontSize;
+    let [size] = fontSize.split('px');
+    size = +size + 5;
+    
+    if (size > 600) {
+        balloon.textContent = "💥";
+    } else {
+        balloon.style.fontSize = `${size}px`;
+    }
+    
+    return size;
+}
+
+
+
+window.addEventListener('keydown', event => {
+    console.log(inflate(balloon));
+})
